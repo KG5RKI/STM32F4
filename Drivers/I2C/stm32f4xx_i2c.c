1e43913dadde895c87faf7c70dea44ffe9e7ce18
Altarf02 <Altarf02@github.com>
2018-06-24 12:21:06 +0530

    2.1

--- Drivers/I2C/stm32f4xx_i2c.c
+++ Drivers/I2C/stm32f4xx_i2c.c
@@ -4,10 +4,10 @@
 #include <stdbool.h>
 
 /* Define GPIO macros */
-#define INPUT  0x0
-#define OUTPUT 0x1
-#define AF     0x2
-#define ANALOG 0x3
+#define INPUT  		0x0
+#define OUTPUT 		0x1
+#define ALT_FUNC	0x2
+#define ANALOG 		0x3
 
 #define PUSH_PULL  0x0
 #define OPEN_DRAIN 0x1
@@ -31,8 +31,8 @@ void I2C1_GPIO_Init()
 	RCC -> AHB1ENR |= (1<<1);
 	
 	/* Set pin mode to AF (Alternate Function) */ 
-	GPIOB->MODER |= (AF<<12);
-	GPIOB->MODER |= (AF<<14);
+	GPIOB->MODER |= (ALT_FUNC<<12);
+	GPIOB->MODER |= (ALT_FUNC<<14);
 	
 	/* Set output type to open-drain */
 	GPIOB->OTYPER |= (1<<6);
@@ -59,8 +59,8 @@ void I2C2_GPIO_Init()
 	RCC -> AHB1ENR |= (1<<1);
 	
 	/* Set pin mode to AF (Alternate Function) */ 
-	GPIOB->MODER |= (AF<<20);
-	GPIOB->MODER |= (AF<<22);
+	GPIOB->MODER |= (ALT_FUNC<<20);
+	GPIOB->MODER |= (ALT_FUNC<<22);
 	
 	/* Set output type to open-drain */
 	GPIOB->OTYPER |= (1<<10);
@@ -88,8 +88,8 @@ void I2C3_GPIO_Init()
 	RCC -> AHB1ENR |= (1<<2);
 	
 	/* Set pin mode to AF (Alternate Function) */ 
-	GPIOA->MODER |= (AF<<16);
-	GPIOC->MODER |= (AF<<18);
+	GPIOA->MODER |= (ALT_FUNC<<16);
+	GPIOC->MODER |= (ALT_FUNC<<18);
 	
 	/* Set output type to open-drain */
 	GPIOA->OTYPER |= (1<<8);
@@ -108,16 +108,13 @@ void I2C3_GPIO_Init()
 	GPIOB->AFR[1] = (AF4<<4);
 }
 
-void I2C1_Master_Init(_Bool NOSTRETCH, _Bool ENGC, uint8_t FREQ)
+void I2C1_Init(_Bool ENGC, uint8_t FREQ)
 {
 	/* Enable I2C1 GPIO pins */
 	I2C1_GPIO_Init();
 	
 	/* Enable I2C1 peripheral clock */
 	RCC->APB1ENR |= (1<<21);
-	
-	/* Configure clock stretching */
-	I2C1->CR1 = (NOSTRETCH==1) ? (I2C1->CR1 | (1<<7)) : (I2C1->CR1 & ~(1<<7));
 	
 	/* Configure general call */
 	I2C1->CR1 = (ENGC==1) ? (I2C1->CR1 | (1<<6)) : (I2C1->CR1 & ~(1<<6));
@@ -136,16 +133,13 @@ void I2C1_Master_Init(_Bool NOSTRETCH, _Bool ENGC, uint8_t FREQ)
 	I2C1->CCR |= 0xA9;
 }
 
-void I2C2_Master_Init(_Bool NOSTRETCH, _Bool ENGC, uint8_t FREQ)
+void I2C2_Init(_Bool ENGC, uint8_t FREQ)
 {
 	/* Enable I2C2 GPIO pins */
 	I2C2_GPIO_Init();
 	
 	/* Configure I2C2 peripheral clock */
 	RCC->APB1ENR |= (1<<22);
-	
-	/* Configure clock stretching */
-	I2C2->CR1 = (NOSTRETCH==1) ? (I2C2->CR1 | (1<<7)) : (I2C2->CR1 & ~(1<<7));
 	
 	/* Configure general call */
 	I2C2->CR1 = (ENGC==1) ? (I2C2->CR1 | (1<<6)) : (I2C2->CR1 & ~(1<<6));
@@ -164,16 +158,13 @@ void I2C2_Master_Init(_Bool NOSTRETCH, _Bool ENGC, uint8_t FREQ)
 	I2C2->CCR |= 0xA9;
 }
 
-void I2C3_Master_Init(_Bool NOSTRETCH, _Bool ENGC, uint8_t FREQ)
+void I2C3_Init(_Bool ENGC, uint8_t FREQ)
 {
-	/* Enable I2C2 GPIO pins */
+	/* Enable I2C3 GPIO pins */
 	I2C3_GPIO_Init();
 	
-	/* Configure I2C2 peripheral clock */
+	/* Configure I2C3 peripheral clock */
 	RCC->APB1ENR |= (1<<23);
-	
-	/* Configure clock stretching */
-	I2C3->CR1 = (NOSTRETCH==1) ? (I2C3->CR1 | (1<<7)) : (I2C3->CR1 & ~(1<<7));
 	
 	/* Configure general call */
 	I2C3->CR1 = (ENGC==1) ? (I2C3->CR1 | (1<<6)) : (I2C3->CR1 & ~(1<<6));
@@ -192,12 +183,72 @@ void I2C3_Master_Init(_Bool NOSTRETCH, _Bool ENGC, uint8_t FREQ)
 	I2C3->CCR |= 0xA9;
 }
 
-void I2Cx_Master_Init(I2Cx n, _Bool NOSTRETCH, _Bool ENGC, uint8_t FREQ)
+void I2Cx_Init(I2Cx n, _Bool ENGC, uint8_t FREQ)
 {
 	switch (n) {
-		case I2C_1: I2C1_Master_Init(NOSTRETCH, ENGC, FREQ);
-		case I2C_2: I2C2_Master_Init(NOSTRETCH, ENGC, FREQ);
-		case I2C_3: I2C3_Master_Init(NOSTRETCH, ENGC, FREQ);
+		case I2C_1: I2C1_Init(ENGC, FREQ);
+		case I2C_2: I2C2_Init(ENGC, FREQ);
+		case I2C_3: I2C3_Init(ENGC, FREQ);
+	}
+}
+
+void I2C1_Slave_Init(_Bool NOSTRETCH, _Bool ENGC, uint8_t FREQ, uint16_t OA)
+{
+	I2Cx_Init(I2C_1, ENGC, FREQ);
+	
+	/* Configure clock stretching */
+	I2C1->CR1 = (NOSTRETCH==1) ? (I2C1->CR1 | (1<<7)) : (I2C1->CR1 & ~(1<<7));
+	
+	/* Set own address */
+	if (OA > 0x7F) {
+		/* 10-bit addressing mode */
+	}
+	else {
+		I2C1->OAR1 &= ~(1<<15);
+		I2C1->OAR1 |= (OA<<1);
+	}
+}
+
+void I2C2_Slave_Init(_Bool NOSTRETCH, _Bool ENGC, uint8_t FREQ, uint16_t OA)
+{
+	I2Cx_Init(I2C_2, ENGC, FREQ);
+	
+	/* Configure clock stretching */
+	I2C2->CR1 = (NOSTRETCH==1) ? (I2C2->CR1 | (1<<7)) : (I2C2->CR1 & ~(1<<7));
+	
+	/* Set own address */
+	if (OA > 0x7F) {
+		/* 10-bit addressing mode */
+	}
+	else {
+		I2C2->OAR1 &= ~(1<<15);
+		I2C2->OAR1 |= (OA<<1);
+	}
+}
+
+void I2C3_Slave_Init(_Bool NOSTRETCH, _Bool ENGC, uint8_t FREQ, uint16_t OA)
+{
+	I2Cx_Init(I2C_3, ENGC, FREQ);
+	
+	/* Configure clock stretching */
+	I2C3->CR1 = (NOSTRETCH==1) ? (I2C3->CR1 | (1<<7)) : (I2C3->CR1 & ~(1<<7));
+	
+	/* Set own address */
+	if (OA > 0x7F) {
+		/* 10-bit addressing mode */
+	}
+	else {
+		I2C3->OAR1 &= ~(1<<15);
+		I2C3->OAR1 |= (OA<<1);
+	}
+}
+
+void I2Cx_Slave_Init(I2Cx n, _Bool NOSTRETCH, _Bool ENGC, uint8_t FREQ, uint16_t OA)
+{
+	switch (n) {
+		case I2C_1: I2C1_Slave_Init(NOSTRETCH, ENGC, FREQ, OA);
+		case I2C_2: I2C2_Slave_Init(NOSTRETCH, ENGC, FREQ, OA);
+		case I2C_3: I2C3_Slave_Init(NOSTRETCH, ENGC, FREQ, OA);
 	}
 }
 
@@ -456,6 +507,111 @@ void I2Cx_GenerateStop(I2Cx n)
 	}
 }
 
+void I2C1_Interrupt_En(I2C_Interrupt ie)
+{
+	switch (ie) {
+		case SB:
+		case ADDR:
+		case ADD10:
+		case STOPF:
+		case BTF:
+			I2C1->CR2 |= (1<<9);
+			NVIC_EnableIRQ(I2C1_EV_IRQn);
+			break;
+		
+		case RxNE:
+		case TxE:
+			I2C1->CR2 |= (1<<10);
+			I2C1->CR2 |= (1<<9);
+			NVIC_EnableIRQ(I2C1_EV_IRQn);
+			break;
+		
+		case BERR:
+		case ARLO:
+		case AF:
+		case OVR:
+		case PECERR:
+		case TIMEOUT:
+		case SMBALERT:
+			I2C1->CR2 |= (1<<8);
+			NVIC_EnableIRQ(I2C1_ER_IRQn);
+			break;
+	}
+}
+
+void I2C2_Interrupt_En(I2C_Interrupt ie)
+{
+	switch (ie) {
+		case SB:
+		case ADDR:
+		case ADD10:
+		case STOPF:
+		case BTF:
+			I2C2->CR2 |= (1<<9);
+			NVIC_EnableIRQ(I2C2_EV_IRQn);
+			break;
+		
+		case RxNE:
+		case TxE:
+			I2C2->CR2 |= (1<<10);
+			I2C2->CR2 |= (1<<9);
+			NVIC_EnableIRQ(I2C2_EV_IRQn);
+			break;
+		
+		case BERR:
+		case ARLO:
+		case AF:
+		case OVR:
+		case PECERR:
+		case TIMEOUT:
+		case SMBALERT:
+			I2C2->CR2 |= (1<<8);
+			NVIC_EnableIRQ(I2C2_ER_IRQn);
+			break;
+	}
+}
+
+void I2C3_Interrupt_En(I2C_Interrupt ie)
+{
+	switch (ie) {
+		case SB:
+		case ADDR:
+		case ADD10:
+		case STOPF:
+		case BTF:
+			I2C3->CR2 |= (1<<9);
+			NVIC_EnableIRQ(I2C3_EV_IRQn);
+			break;
+		
+		case RxNE:
+		case TxE:
+			I2C3->CR2 |= (1<<10);
+			I2C3->CR2 |= (1<<9);
+			NVIC_EnableIRQ(I2C3_EV_IRQn);
+			break;
+		
+		case BERR:
+		case ARLO:
+		case AF:
+		case OVR:
+		case PECERR:
+		case TIMEOUT:
+		case SMBALERT:
+			I2C3->CR2 |= (1<<8);
+			NVIC_EnableIRQ(I2C3_ER_IRQn);
+			break;
+	}
+}
+
+void I2Cx_Interrupt_En(I2Cx n, I2C_Interrupt ie)
+{
+	switch (n) {
+		case I2C_1: I2C1_Interrupt_En(ie);
+		case I2C_2: I2C2_Interrupt_En(ie);
+		case I2C_3: I2C3_Interrupt_En(ie);
+	}
+}
+
 void I2C1_Master_Transmit(uint16_t addr, uint8_t *data, int N)
 {
 	/* Generate START condition */
@@ -540,8 +696,56 @@ void I2Cx_Master_Transmit(I2Cx n, uint16_t addr, uint8_t *data, int N)
 	}
 }
 
+uint8_t I2C1_Read_From_DR()
+{
+	uint8_t data;
+	while (1) {
+		if ((I2C1->SR1>>6) & 0x1) {
+			data = I2C1->DR;
+			break;
+		}
+	}
+	return data;
+}
+
+uint8_t I2C2_Read_From_DR()
+{
+	uint8_t data;
+	while (1) {
+		if ((I2C2->SR1>>6) & 0x1) {
+			data = I2C2->DR;
+			break;
+		}
+	}
+	return data;
+}
+
+uint8_t I2C3_Read_From_DR()
+{
+	uint8_t data;
+	while (1) {
+		if ((I2C3->SR1>>6) & 0x1) {
+			data = I2C3->DR;
+			break;
+		}
+	}
+	return data;
+}
+
+uint8_t I2Cx_Read_From_DR(I2Cx n)
+{
+	switch (n) {
+		case I2C_1: return I2C1_Read_From_DR();
+		case I2C_2: return I2C2_Read_From_DR();
+		case I2C_3: return I2C3_Read_From_DR();
+	}
+	return '\0';
+}
+
 uint8_t I2C1_Master_Receive(uint16_t addr)
 {
+	uint8_t data;
+	
 	/* Generate START condition */
 	I2Cx_GenerateSTART(I2C_1);
 	
@@ -553,12 +757,87 @@ uint8_t I2C1_Master_Receive(uint16_t addr)
 		I2Cx_Send7bitAddressR(I2C_1, addr);
 	}
 	
+	data = I2Cx_Read_From_DR(I2C_1);
+	
+	/* Disable ACK */
+	I2Cx_ACK_Dis(I2C_1);
+	
+	/* Generate STOP condition */
+	I2Cx_GenerateStop(I2C_1);
+	
+	return data;
 }
 
-uint8_t I2C1_Master_Receive2byte()
+uint8_t I2C2_Master_Receive(uint16_t addr)
+{
+	uint8_t data;
+	
+	/* Generate START condition */
+	I2Cx_GenerateSTART(I2C_2);
+	
+	/* Send slave address */
+	if (addr > 0x7F) {
+		//10-bit addressing mode
+	}
+	else {
+		I2Cx_Send7bitAddressR(I2C_2, addr);
+	}
+	
+	data = I2Cx_Read_From_DR(I2C_2);
+	
+	/* Disable ACK */
+	I2Cx_ACK_Dis(I2C_2);
+	
+	/* Generate STOP condition */
+	I2Cx_GenerateStop(I2C_2);
+	
+	return data;
+}
+
+uint8_t I2C3_Master_Receive(uint16_t addr)
+{
+	uint8_t data;
+	
+	/* Generate START condition */
+	I2Cx_GenerateSTART(I2C_3);
+	
+	/* Send slave address */
+	if (addr > 0x7F) {
+		//10-bit addressing mode
+	}
+	else {
+		I2Cx_Send7bitAddressR(I2C_3, addr);
+	}
+	
+	data = I2Cx_Read_From_DR(I2C_3);
+	
+	/* Disable ACK */
+	I2Cx_ACK_Dis(I2C_3);
+	
+	/* Generate STOP condition */
+	I2Cx_GenerateStop(I2C_3);
+	
+	return data;
+}
+
+uint8_t I2Cx_Master_Receive(I2Cx n, uint16_t addr)
+{
+	switch (n) {
+		case I2C_1: return I2C1_Master_Receive(addr);
+		case I2C_2: return I2C2_Master_Receive(addr);
+		case I2C_3: return I2C3_Master_Receive(addr);
+	}
+	return '\0';
+}
+
+uint8_t I2C1_Slave_Receive()
 {
 }
 
-uint8_t I2C1_Master_Receivenbyte()
+uint8_t I2C2_Slave_Receive()
+{
+}
+
+uint8_t I2C3_Slave_Receive()
 {
 }
--- Drivers/I2C/stm32f4xx_i2c.h
+++ Drivers/I2C/stm32f4xx_i2c.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 enum I2Cx {I2C_1, I2C_2, I2C_3};
 typedef enum I2Cx I2Cx;
 
@@ -7,7 +9,26 @@ enum I2C_Mode {
 	MASTER_TRANSMITTER,
 	MASTER_RECEIVER
 };
+
+enum I2C_Interrupt {
+	SB,			//Start bit sent
+	ADDR,		//Address sent(master)/matched(slave)
+	ADD10,	//10-bit header sent(slave)
+	STOPF,	//Stop received(master)
+	BTF,		//Byte transfer finished
+	RxNE,		//Receive buffer not empty
+	TxE,		//Transmit buffer empty
+	BERR,		//Bus error
+	ARLO,		//Arbitration loss(master)
+	AF,			//Acknowledge failure
+	OVR,		//Overrum/Underrun
+	PECERR,	//PEC error
+	TIMEOUT,//Timeout error
+	SMBALERT//SMBus alert
+};
+
+typedef enum I2C_Interrupt I2C_Interrupt;
 	
-extern void I2Cx_Init(I2Cx);
+extern void I2Cx_Init(I2Cx, _Bool, uint8_t);
 extern void I2Cx_En(I2Cx);
 extern void I2Cx_Dis(I2Cx);
