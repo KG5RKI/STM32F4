826d952de00c0948418b7f9090711a3d61313646
Altarf02 <Altarf02@github.com>
2018-06-27 12:06:25 +0530

    2.3

--- Examples/CS43L22 (Audio DAC)/CS43L22.c
+++ Examples/CS43L22 (Audio DAC)/CS43L22.c
@@ -2,44 +2,44 @@
 
 #define CS43L22_DEVICE_ADDR (uint8_t)0x4A
 
-#define CHIP_ID_REG									(uint8_t)0x01
-#define POWER_CONTROL_1 						(uint8_t)0x02
-#define POWER_CONTROL_2 						(uint8_t)0x04
-#define CLOCKING_CONTROL						(uint8_t)0x05
-#define INTERFACE_CONTROL_1					(uint8_t)0x06
-#define INTERFACE_CONTROL_2					(uint8_t)0x07
-#define PASS_A											(uint8_t)0x08
-#define PASS_B											(uint8_t)0x09
-#define ANALOG_ZC_SR								(uint8_t)0x0A
-#define PASSTHROUGH_GANG_CONTROL		(uint8_t)0x0C
-#define PLAYBACK_CONTROL_1					(uint8_t)0x0D
-#define MISC_CONTROLS								(uint8_t)0x0E
-#define PLAYBACK_CONTROL_2					(uint8_t)0x0F
-#define PASS_A_VOLUME								(uint8_t)0x14
-#define PASS_B_VOLUME								(uint8_t)0x15
-#define PCMA_VOLUME									(uint8_t)0x1A
-#define PCMB_VOLUME									(uint8_t)0x1B
-#define BEEP_FREQUENCY_AND_ON_TIME	(uint8_t)0x1C
-#define BEEP_VOLUME_AND_OFF_TIME		(uint8_t)0x1D
-#define BEEP_AND_TONE_CONFIG				(uint8_t)0x1E
-#define TONE_CONTROL								(uint8_t)0x1F
-#define MASTER_VOLUME_CONTROL_A			(uint8_t)0x20
-#define MASTER_VOLUME_CONTROL_B			(uint8_t)0x21
-#define HEADPHONE_VOLUME_CONTROL_A	(uint8_t)0x22
-#define HEADPHONE_VOLUME_CONTROL_B	(uint8_t)0x23
-#define SPEAKER_VOLUME_CONTROL_A		(uint8_t)0x24
-#define SPEAKER_VOLUME_CONTROL_B		(uint8_t)0x25
-#define PCM_CHANNEL_SWAP						(uint8_t)0x26
-#define LIMITER_CONTROL_1						(uint8_t)0x27
-#define LIMITER_CONTROL_2						(uint8_t)0x28
-#define LIMITER_ATTACK_RATE					(uint8_t)0x29
-#define STATUS											(uint8_t)0x2E
-#define BATTERY_COMPENSATION				(uint8_t)0x2F
-#define VP_BATTERY_LEVEL						(uint8_t)0x30
-#define SPEAKER_STATUS							(uint8_t)0x31
-#define TEMPERATURE_MONITOR_CONTROL	(uint8_t)0x32
-#define THERMAL_FOLDBACK						(uint8_t)0x33
-#define CHARGE_PUMP_FREQUENCY				(uint8_t)0x34
+#define CHIP_ID_REG                 (uint8_t)0x01
+#define POWER_CONTROL_1             (uint8_t)0x02
+#define POWER_CONTROL_2             (uint8_t)0x04
+#define CLOCKING_CONTROL            (uint8_t)0x05
+#define INTERFACE_CONTROL_1         (uint8_t)0x06
+#define INTERFACE_CONTROL_2         (uint8_t)0x07
+#define PASS_A                      (uint8_t)0x08
+#define PASS_B                      (uint8_t)0x09
+#define ANALOG_ZC_SR                (uint8_t)0x0A
+#define PASSTHROUGH_GANG_CONTROL    (uint8_t)0x0C
+#define PLAYBACK_CONTROL_1          (uint8_t)0x0D
+#define MISC_CONTROLS               (uint8_t)0x0E
+#define PLAYBACK_CONTROL_2          (uint8_t)0x0F
+#define PASS_A_VOLUME               (uint8_t)0x14
+#define PASS_B_VOLUME               (uint8_t)0x15
+#define PCMA_VOLUME                 (uint8_t)0x1A
+#define PCMB_VOLUME                 (uint8_t)0x1B
+#define BEEP_FREQUENCY_AND_ON_TIME  (uint8_t)0x1C
+#define BEEP_VOLUME_AND_OFF_TIME    (uint8_t)0x1D
+#define BEEP_AND_TONE_CONFIG        (uint8_t)0x1E
+#define TONE_CONTROL                (uint8_t)0x1F
+#define MASTER_VOLUME_CONTROL_A     (uint8_t)0x20
+#define MASTER_VOLUME_CONTROL_B     (uint8_t)0x21
+#define HEADPHONE_VOLUME_CONTROL_A  (uint8_t)0x22
+#define HEADPHONE_VOLUME_CONTROL_B  (uint8_t)0x23
+#define SPEAKER_VOLUME_CONTROL_A    (uint8_t)0x24
+#define SPEAKER_VOLUME_CONTROL_B    (uint8_t)0x25
+#define PCM_CHANNEL_SWAP            (uint8_t)0x26
+#define LIMITER_CONTROL_1           (uint8_t)0x27
+#define LIMITER_CONTROL_2           (uint8_t)0x28
+#define LIMITER_ATTACK_RATE         (uint8_t)0x29
+#define STATUS                      (uint8_t)0x2E
+#define BATTERY_COMPENSATION        (uint8_t)0x2F
+#define VP_BATTERY_LEVEL            (uint8_t)0x30
+#define SPEAKER_STATUS              (uint8_t)0x31
+#define TEMPERATURE_MONITOR_CONTROL (uint8_t)0x32
+#define THERMAL_FOLDBACK            (uint8_t)0x33
+#define CHARGE_PUMP_FREQUENCY       (uint8_t)0x34
 
 void CS43L22_GPIO_Init()
 {
@@ -135,6 +135,21 @@ void CS43L22_I2S3_Init()
 	SPI3->I2SCFGR |= (1<<10); //I2S Enable
 }
 
+void I2C1_WaitUntilPeripheralFree()
+{
+	while ((I2C1->SR2>>1) & 0x1);
+}
+
+void I2C1_WaitUntilTxRegEmpty()
+{
+	while (!((I2C1->SR1 >> 7) & 0x1));
+}
+
+void I2C1_WaitUntilRxRegNotEmpty()
+{
+	while (!((I2C1->SR1 >> 6) & 0x1));
+}
+
 void I2C1_GenerateSTART()
 {
 	I2C1->CR1 |= (1<<8);				//Generate START condition
@@ -167,18 +182,28 @@ void I2C1_GenerateStop()
 	I2C1->CR1 |= (1<<9);								//Generate STOP condition
 }
 
+void I2C1_Set_DR(uint8_t data)
+{
+	I2C1->DR = data;
+}
+
+uint8_t I2C1_Get_DR()
+{
+	uint8_t data = I2C1->DR;
+	return data;
+}
+
 void CS43L22_I2C1_Write(uint8_t device_addr, uint8_t MAP_byte, uint8_t data)
 {
 	I2C1_GenerateSTART();
 	I2C1_Send7bitAddressW(device_addr);
 	
-	while (!((I2C1->SR1 >> 7) & 0x1));			//Send MAP byte, auto-increment off
-	I2C1->DR = MAP_byte;
-	/* Check ACK */
+	I2C1_WaitUntilTxRegEmpty();							//Send MAP byte, auto-increment off
+	I2C1_Set_DR(MAP_byte);
 	
-	while (!((I2C1->SR1 >> 7) & 0x1));			//Send data to be written
-	I2C1->DR = data;
-	/* Check ACK */
+	I2C1_WaitUntilTxRegEmpty();							//Send data to be written
+	I2C1_Set_DR(data);
+	
 	I2C1_GenerateStop();
 }
 
@@ -187,19 +212,25 @@ uint8_t CS43L22_I2C1_AbortedWrite(uint8_t device_addr, uint8_t MAP_byte)
 	uint8_t rec_data;
 	
 	/* Read a particular register by aborted write method */
+	I2C1_WaitUntilPeripheralFree();					//Wait until the BUSY bit is cleared
 	I2C1_GenerateSTART();										//Generate START condition
+	
 	I2C1_Send7bitAddressW(device_addr);			//Send device address, with W bit
 	
-	while (!((I2C1->SR1 >> 7) & 0x1));			//Send MAP byte, auto-increment off
-	I2C1->DR = MAP_byte;
-	/* Check ACK */
+	I2C1_WaitUntilTxRegEmpty();					//Send MAP byte, auto-increment off
+	I2C1_Set_DR(MAP_byte);
+	
 	I2C1_GenerateStop();
+	
 	I2C1_GenerateSTART();
+	
 	I2C1_Send7bitAddressR(device_addr);			//Send the device address, with R bit
-	/* Check ACK */
-	rec_data = I2C1->DR;
-	/*Send ACK bit */
+	
+	I2C1_WaitUntilRxRegNotEmpty();
+	rec_data = I2C1_Get_DR();
+	
 	I2C1_GenerateStop();
+	
 	return rec_data;
 }
 
@@ -238,19 +269,60 @@ void CS43L22_PowerUp()
 	/* 1. Reset CS43L22 by toggling PD4 */
 	GPIO_Toggle2_With_Delay(GPIOD, 4);
 	
-	/* Start MCLK with appropriate frequency */
+	/* 2. Start MCLK with appropriate frequency - MCLK 12Mhz, Sample rate 8.000 */
+	uint8_t clock_settings = 0;
+	clock_settings &= ~(1<<0);	//MCLK Divide By 2
+	clock_settings |= (1<<1);		//Internal MCLK/LRCK Ratio
+	clock_settings &= ~(1<<3);	//27 MHz Video Clock
+	clock_settings |= (1<<4);		//32kHz Sample Rate Group
+	clock_settings |= (3<<5);		//Speed Mode
+	CS43L22_I2C1_Write(CS43L22_DEVICE_ADDR, CLOCKING_CONTROL, clock_settings);
 	
-	/* Set POWER_CONTROL_1 register to 0x9E */
+	/* 3. Set POWER_CONTROL_1 register to 0x9E */
+	CS43L22_I2C1_Write(CS43L22_DEVICE_ADDR, POWER_CONTROL_1, 0x9E);
 	
-	/* Apply LRCK, SCLK and SDIN for normal operation to begin */
+	/* 4. Apply WS, SCLK and SDIN for normal operation to begin */
 	CS43L22_I2S3_Init();
 }
 
 void CS43L22_PowerDown()
 {
 	/* 1. Mute the DACs */
+	
 	/* 2. Set POWER_CONTROL_1 register to 0x02 */
+	CS43L22_I2C1_Write(CS43L22_DEVICE_ADDR, POWER_CONTROL_1, 0x02);
+	
 	/* 3. Set ~RESET to LOW */
+	GPIOD->BSRR |= (1<<4);
+}
+
+void CS43L22_Configure()
+{
+	/* Interface control 1*/
+	uint8_t ic1_data = 0;
+	ic1_data |= (1<<2);		//DAC Interface Format - I2S
+	ic1_data &= ~(1<<7);	//Slave(input only)
+	CS43L22_I2C1_Write(CS43L22_DEVICE_ADDR, INTERFACE_CONTROL_1, ic1_data);
+}
+
+void CS43L22_Headphone_Mute()
+{
+}
+
+void CS43L22_Headphone_Unmute()
+{
+}
+
+void CS43L22_Speaker_Mute()
+{
+}
+
+void CS43L22_Speaker_Unmute()
+{
+}
+
+void CS43L22_Master_Volume_Control()
+{
 }
 
 int main(void)
@@ -261,16 +333,19 @@ int main(void)
 	LED_GPIO_Init();
 	CS43L22_I2C1_Init();
 	CS43L22_PowerUp();
+	CS43L22_Configure();
 	
 	/* Read Chip ID */
 	uint8_t chip_id = CS43L22_I2C1_AbortedWrite(CS43L22_DEVICE_ADDR, CHIP_ID_REG);
-	if (chip_id == 0x1C) {
+	if ((chip_id>>3) == 0x1C) {
 		LED_Toggle(12);
 	}
 	else {
 		LED_On(14);
 		goto stop;
 	}
+	
+	/* Beep Generator */
 	
 	stop: while (1) {
 	}
